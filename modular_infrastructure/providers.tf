4ebf4a32399165c70411298bc63d0ad6bb58e955
vishaalpal <vishaalvpal@gmail.com>
2024-02-05 05:18:24 +1100

    Updating aws credentials

--- modular_infrastructure/providers.tf
+++ modular_infrastructure/providers.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region                  = "ap-southeast-2"
-  shared_credentials_file = "/Users/vishaalpal/.aws/credentials"
+  shared_config_files = "Users/vishaalpal/.aws/config"
+  shared_credentials_files = "/Users/vishaalpal/.aws/credentials"
   profile                 = "vishaalpal"
 }
